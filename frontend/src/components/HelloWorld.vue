e9d24b10ccac333fb32519a5b476f467888c00a3
fattakhoval <fattahoval66@gmail.com>
2024-12-01 16:26:58 +0300

    new methods for trans and url

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -71,7 +71,7 @@ export default {
       console.log('Signing up with:', this.signupForm);
 
       // Отправка данных на сервер
-      fetch('http://localhost/signup', {
+      fetch('http://localhost:8485/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -97,7 +97,7 @@ export default {
       console.log('Signing in with:', this.signinForm);
 
       // Отправка данных на сервер
-      fetch('https://your-api-endpoint.com/signin', {
+      fetch('http://localhost:8485/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
